3e63be94e1003814e545300c31d9307dda1aec06
daniela <garfieldlamona@hotmail.com>
2013-04-25 10:58:26 -0500

    data base yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tabla::Application.config.secret_token = 'b75ef2cb3f5fe2f15f7e27726d9efbd5f73311e319e9cfef5c0b4c8e0ee217a1d18f8793d5328f280257f6563e8043aa36b24643d13f6e47d0db87d50fbde5b3'
